e359bd24eb4909f5756b2043d864290cad2c6016
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2025-01-17 15:15:14 +0300

    added module & types

--- src/myModule.d.ts
+++ src/myModule.d.ts
@@ -1,1 +1,3 @@
-// types
+export declare function add(a: number, b: number): number;
+
+export declare function greet(name: string): string;
--- src/myModule.js
+++ src/myModule.js
@@ -1,1 +1,20 @@
-// module
+/**
+ * Returns the sum of two numbers.
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+function add(a, b) {
+    return a + b;
+}
+
+/**
+ * Returns a greeting message.
+ * @param {string} name
+ * @returns {string}
+ */
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+module.exports = { add, greet };
